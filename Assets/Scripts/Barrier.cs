8e4ee357ec9466580d92c163e475c63e7cecfc5b
Yan Karlo <yankarlo@hotmail.com>
2019-04-01 22:47:38 -0400

    Scoreboard Initial Tries

---
+++ Assets/Scripts/Barrier.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrier : MonoBehaviour
+{
+    public int Damage=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setDamage()
+    {
+        Damage++;
+        renderDamage(Damage);
+    }
+
+    public int getDamage()
+    {
+        return Damage;
+    }
+
+    public void renderDamage(int value)
+    {
+
+    }
+}
--- Assets/Scripts/BulletInventory.cs
+++ Assets/Scripts/BulletInventory.cs
@@ -7,6 +7,8 @@ public class BulletInventory : MonoBehaviour
     public string  trigger;
     private GameController gCont;
     private Assets.Scripts.Objects.Tank Tank01, Tank02;
+    public GameObject brick2;
+    public GameObject brick3;
 
     void Start()
     {
@@ -24,7 +26,22 @@ public class BulletInventory : MonoBehaviour
         switch (other.tag)
         {
             case "Barrier":
+                Debug.Log(other.ToString());
                 Debug.Log("Barrier is gone");
+                Instantiate(brick2, other.transform.position, other.transform.rotation);
+                Destroy(other.gameObject);
+                Destroy(this.gameObject);
+                break;
+            case "Barrier2":
+                Debug.Log(other.ToString());
+                Debug.Log("Barrier2 is gone");
+                Instantiate(brick3, other.transform.position, other.transform.rotation);
+                Destroy(other.gameObject);
+                Destroy(this.gameObject);
+                break;
+            case "Barrier3":
+                Debug.Log(other.ToString());
+                Debug.Log("Barrier3 is gone");
                 Destroy(other.gameObject);
                 Destroy(this.gameObject);
                 break;
@@ -32,13 +49,11 @@ public class BulletInventory : MonoBehaviour
                 gCont.SetPlayer2Points();
                 Debug.Log("Player 2 : " + gCont.Player2Points);
                 Destroy(this.gameObject);
-                //GameObject.FindGameObjectWithTag("Player1").GetComponent<Player1>().Tank.SetDamage();
                 Tank01.SetDamage();
                 break;
             case "player2":
                 gCont.SetPlayer1Points();
                 Debug.Log("Player 1 : " + gCont.Player1Points);
-                //GameObject.FindGameObjectWithTag("Player2").GetComponent<Player2>().Tank.SetDamage();
                 Destroy(this.gameObject);
                 Tank02.SetDamage();
                 break;
