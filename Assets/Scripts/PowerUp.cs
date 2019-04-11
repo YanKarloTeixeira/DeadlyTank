0a443eecee3a777da5353fbd4fe59dd9090176a5
Yan Karlo <yankarlo@hotmail.com>
2019-04-10 21:36:30 -0400

    PowerUp preparation

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,61 @@
+﻿using Assets.Scripts.Objects;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public string trigger;
+    private GameController gCont;
+    private Assets.Scripts.Objects.Tank Tank01, Tank02;
+    public GameObject Tank01_;
+    public int effect = 0;
+    public int type = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject,Random.Range(10f,15f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if 'other' is a tagged an item
+        Debug.Log("Other tag: " + other.tag);
+        Debug.Log("Bullet trigger: " + this.trigger);
+        if (other.tag == this.trigger) return;
+        switch (other.tag.ToUpper())
+        {
+            case "PLAYER1" :
+
+                Tank02.ScorePoints();
+                Tank01.SetDamage();
+                gCont.setScoreboard(Tank02.Flag);
+                Destroy(this.gameObject);
+                break;
+            case "PLAYER2":
+                Tank01.ScorePoints();
+                Tank02.SetDamage();
+                gCont.setScoreboard(Tank01.Flag);
+                Destroy(this.gameObject);
+                break;
+        }
+    }
+
+    void PowerUpEffect(Tank tankTrigger, Tank tankOpponent)
+    {
+        switch (type)
+        {
+            case 1: // gas
+                tankTrigger.setFuel(tankTrigger.GetFuel()+effect);
+                break;
+        }
+    }
+
+}
---
+++ Assets/Scripts/PowerUpController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+
+public class PowerUpController : MonoBehaviour
+{
+    public GameObject Gas;
+    public GameObject Speed;
+    public GameObject Recover;
+    public GameObject LandMine;
+    public int GasQty=2;
+    public int SpeedQty=2;
+    public int RecoverQty=2;
+    public int LandMineQty=7;
+    public int GasFactor = 0;
+    public int SpeedFactor = 0;
+    public int RecoverFactor = 0;
+    public int LandMineFactor = 0;
+
+
+    System.Timers.Timer aTimer;
+    public bool isVisible = false;
+    public SpriteRenderer sprite = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //aTimer = new System.Timers.Timer();
+        //aTimer.Interval = Random.Range(10f, 15f);
+        //sprite = PowerUp.GetComponent<GameController>()
+        PowerUpCreation();
+        // InvokeRepeating("InstantiatingPowerup", Random.Range(1.0f, 10.0f), )
+        //CancelInvoke("InstantiatePowerup");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    private void OnTimedEvent(object sender, ElapsedEventArgs e)
+    {
+    }
+
+
+    //private void InstantiatePowerup()
+    //{
+    //    // Instantiate the object
+    
+
+    //    // Determine location on my map
+    //    // Map width, map height
+    //    // myPowerup.transform.position
+    //}
+
+    private void PowerUpCreation()
+    {
+        for (int i = 0; i < GasQty; i++) Invoke("InstantiateGasPowerup", UnityEngine.Random.Range(1.0f, 10.0f));
+        for (int i = 0; i < SpeedQty; i++) Invoke("InstantiateSpeedPowerup", UnityEngine.Random.Range(1.0f, 10.0f));
+        for (int i = 0; i < RecoverQty; i++) Invoke("InstantiateRecoverPowerup", UnityEngine.Random.Range(1.0f, 10.0f));
+        for (int i = 0; i < LandMineQty; i++) Invoke("InstantiateLandMinePowerup", UnityEngine.Random.Range(1.0f, 10.0f));
+
+
+
+    }
+
+    private void InstantiateGasPowerUp()
+    {
+        //Vector3 v3 = new Vector3(100f,100f);
+        //GameObject gGasObj = Instantiate(Gas,v3);
+    }
+}
